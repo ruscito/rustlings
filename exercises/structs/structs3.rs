78dd0f6fabd74a81401ef9b8fde68f5d25e701f8
Agostino Ruscito <ruscito@gmail.com>
2021-09-12 19:00:33 -0500

    structs completed

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,7 +3,6 @@
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,18 +14,21 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Weight cant be negative");
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        ! self.sender_country.eq(&self.recipient_country)
+    
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
         // Something goes here... (beware of grams to kg conversion)
+        let weight_in_kg = (self.weight_in_grams / 1000) as i32;
+        weight_in_kg * cents_per_kg
     }
 }
 
@@ -58,7 +60,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
+        let cents_per_kg = 4500;
         
         let package = Package::new(sender_country, recipient_country, 1500);
         
