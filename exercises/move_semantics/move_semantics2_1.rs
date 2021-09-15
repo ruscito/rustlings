6a0079df00745d7e161d812e13027e8e827394c3
Agostino Ruscito <ruscito@gmail.com>
2021-09-14 22:56:53 -0500

    Completed move_semactics

--- exercises/move_semantics/move_semantics2_1.rs
+++ exercises/move_semantics/move_semantics2_1.rs
@@ -4,9 +4,11 @@
 
 
 fn main() {
-    let mut vec0 = Vec::new();
+    let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(&mut vec0);
+    let other_vec0 = vec0.clone();
+
+    let mut vec1 = fill_vec(other_vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -16,9 +18,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
     vec.push(22);
     vec.push(44);
     vec.push(66);
-    vec.clone()
+
+    vec
 }
--- exercises/move_semantics/move_semantics2_2.rs
+++ exercises/move_semantics/move_semantics2_2.rs
@@ -1,7 +1,7 @@
-// move_semantics3.rs
-// Make me compile without adding new lines-- just changing existing lines!
-// (no lines with multiple semicolons necessary!)
-// Execute `rustlings hint move_semantics3` for hints :)
+// move_semantics2.rs
+// Make me compile without changing line 13!
+// Execute `rustlings hint move_semantics2` for hints :)
+
 
 
 fn main() {
@@ -9,7 +9,8 @@ fn main() {
 
     let mut vec1 = fill_vec(&mut vec0);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
@@ -20,6 +21,5 @@ fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
     vec.clone()
 }
