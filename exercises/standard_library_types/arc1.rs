bed6aa312d30081eda6e1a4daa8274470aad3a42
Agostino Ruscito <ruscito@gmail.com>
2021-09-18 18:27:50 -0500

    Completed to factorial

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -4,7 +4,6 @@
 // somewhere. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` for hints :)
 
-// I AM NOT DONE
 
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
@@ -12,16 +11,17 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers); // this create a safe thread reference of numbers
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
+        let child_numbers = Arc::clone(&shared_numbers); // this create a clone so a counted reference rather than a deep copy
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
             while i < child_numbers.len() {
                 sum += child_numbers[i];
-                i += 5;
+                i += 8;
             }
             println!("Sum of offset {} is {}", offset, sum);
         }));
--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,12 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
+
+use crate::List::{Cons, Nil};
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -30,11 +31,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    let list = Nil;
+    list
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    list
 }
 
 #[cfg(test)]
